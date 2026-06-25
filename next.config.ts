53777e62bfa7f84edf5d0681d547f8094e20c42b
nitinramrathod <you@example.com>
2026-06-25 10:20:31 +0530

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,45 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       { protocol: "https", hostname: "images.unsplash.com" },
+//       { protocol: "https", hostname: "randomuser.me" },
+//     ],
+//   },
+//   experimental: {
+//     optimizePackageImports: ["lucide-react", "framer-motion"],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "randomuser.me" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
     ],
   },
+
   experimental: {
     optimizePackageImports: ["lucide-react", "framer-motion"],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig as any);
